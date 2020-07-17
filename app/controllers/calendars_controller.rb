d91856f9f3015d35f5f2f3e5a964de09ed568b79
kazuyukirumaruma <67451150+kazuyukirumaruma@users.noreply.github.com>
2020-07-17 19:32:31 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,24 +19,32 @@ class CalendarsController < ApplicationController
   end
 
   def get_week
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
+    @wdays = ['','(月)','(火)','(水)','(木)','(金)','(土)','(日)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day: 1日
-
     @week_days = []
-
     @plans = Plan.where(date: @todays_date..@todays_date + 7)
-
+    wdays = []
     7.times do |x|
+      day = @wdays[(@todays_date + x).cwday]
       plans = []
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: plans, day: day}
       @week_days.push(days)
     end
 
   end
 end
+
+
+
+# date: 7/16
+# push >>>>> @week_days[{date: 7/16}]
+
+# date: 7/17
+# push >>>>> @week_days[{date: 7/16}, {date:7/17}]
+
